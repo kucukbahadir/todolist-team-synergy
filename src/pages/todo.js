06e04657436343f17ec210985eaa7d94fc7f434e
Christian <118530836+faze-chris@users.noreply.github.com>
2024-09-27 13:45:20 +0200

    the list of to do main page
    i made the main page where you can see all the to do's

--- src/pages/todo.js
+++ src/pages/todo.js
@@ -3,89 +3,102 @@ import React from "react";
 // Due to the router being nested in "App-main", it also takes its CSS, found in "App.css"
 const Todo = () => {
     return (
-        <div class="container-fluid">
-            <div class="row justify-content-center align-items-center g-2">
-                <div class="col-3">
-                    <div class="card">
-                        <div class="card-body">
-                            <h4 class="card-title">To do 1</h4>
-                            <p class="card-text">Text</p>
+        <div>
+            <div className="container-fluid bg-black rounded-pill">
+                <br></br>
+                    <div className="row justify-content-center align-items-center text-center">
+                        <div className="col"></div>
+                        <div className="col">
+                            <div className="underline p-2">
+                                <h2 className="tx-pro">todo lists</h2>
+                            </div>
+                        </div>
+                        <div className="col"></div>
+                    </div>
+                <br></br>
+            </div>
+            <br></br>
+            <div className="container-xl">
+                    <div className="row justify-content-center align-items-center text-center">
+                        <div className="col-4"></div>
+                        <div className="col-4">
+                        <a href="https://www.playstation.com/nl-nl/games/the-last-of-us-part-ii-remastered/"
+                           className="btn btn-outline-success">Add todolist</a>
+                        </div>
+                        <div className="col-4"></div>
+                    </div>
+            </div>
+            <br></br>
+            <div className="container-fluid">
+                <div className="row justify-content-center align-items-center g-2">
+                    <div className="col-3">
+                        <div className="card rounded-pill">
+                            <div className="card-body">
+                                <h4 className="card-title">To do 1</h4>
+                                <p className="card-text">I need to add some things to this page</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-3">
-                    <div class="card">
-                        <img class="card-img-top" src="holder.js/100x180/" alt="Title" />
-                        <div class="card-body">
-                            <h4 class="card-title">To do 1</h4>
-                            <p class="card-text">Text</p>
+                    <div className="col-3">
+                        <div className="card rounded-pill">                      
+                            <div className="card-body">
+                                <h4 className="card-title">To do 2</h4>
+                                <p className="card-text">Text</p>
+                            </div>
                         </div>
                     </div>
-                </div>  <div class="col-3">
-                    <div class="card">
-                        <img class="card-img-top" src="holder.js/100x180/" alt="Title" />
-                        <div class="card-body">
-                            <h4 class="card-title">To do 1</h4>
-                            <p class="card-text">Text</p>
+                    <div className="col-3">
+                        <div className="card rounded-pill">                      
+                            <div className="card-body">
+                                <h4 className="card-title">To do 3</h4>
+                                <p className="card-text">Text</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-3">
-                    <div class="card">
-                        <img class="card-img-top" src="holder.js/100x180/" alt="Title" />
-                        <div class="card-body">
-                            <h4 class="card-title">To do 1</h4>
-                            <p class="card-text">Text</p>
+                    <div className="col-3">
+                        <div className="card rounded-pill">                      
+                            <div className="card-body">
+                                <h4 className="card-title">To do 4</h4>
+                                <p className="card-text">Text</p>
+                            </div>
+                        </div>
+                    </div>  
+                    <div className="col-3">
+                        <div className="card rounded-pill">                     
+                            <div className="card-body">
+                                <h4 className="card-title">To do 5</h4>
+                                <p className="card-text">Text</p>
+                            </div>
                         </div>
                     </div>
-                </div>  <div class="col-3">
-                    <div class="card">
-                        <img class="card-img-top" src="holder.js/100x180/" alt="Title" />
-                        <div class="card-body">
-                            <h4 class="card-title">To do 1</h4>
-                            <p class="card-text">Text</p>
+                    <div className="col-3">
+                        <div className="card rounded-pill">                    
+                            <div className="card-body">
+                                <h4 className="card-title">To do 6</h4>
+                                <p className="card-text">Text</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-3">
-                    <div class="card">
-                        <img class="card-img-top" src="holder.js/100x180/" alt="Title" />
-                        <div class="card-body">
-                            <h4 class="card-title">To do 1</h4>
-                            <p class="card-text">Text</p>
+                    <div className="col-3">
+                        <div className="card rounded-pill">
+                            <div className="card-body">
+                                <h4 className="card-title">To do 7</h4>
+                                <p className="card-text">Text</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-3">
-                    <div class="card">
-                        <img class="card-img-top" src="holder.js/100x180/" alt="Title" />
-                        <div class="card-body">
-                            <h4 class="card-title">To do 1</h4>
-                            <p class="card-text">Text</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-3">
-                    <div class="card">
-                        <img class="card-img-top" src="holder.js/100x180/" alt="Title" />
-                        <div class="card-body">
-                            <h4 class="card-title">To do 1</h4>
-                            <p class="card-text">Text</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-3">
-                    <div class="card">
-                        <img class="card-img-top" src="holder.js/100x180/" alt="Title" />
-                        <div class="card-body">
-                            <h4 class="card-title">To do 1</h4>
-                            <p class="card-text">Text</p>
+                    <div className="col-3">
+                        <div className="card rounded-pill">
+                            <div className="card-body">
+                                <h4 className="card-title">To do 8</h4>
+                                <p className="card-text">Text</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-    	</div>
+        </div>
     );
 };
- 
+
 export default Todo;
