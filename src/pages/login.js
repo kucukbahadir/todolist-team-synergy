5abfdf1fc099f4aa903278ab00313ef2acf626fe
MartenYeager <marten.ter.meer@gmail.com>
2024-09-28 13:15:32 +0200

    Little clean-up
    - Added placeholder in input
    - Removed old rerender system

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,28 +1,20 @@
 import React from "react";
-import {useState} from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
-//import Todo from "./todo"
 
 function Login() {
     const [name, setName] = useState("");
-    //const [auth, setAuth] = useState(false);
     const nav = useNavigate();
 
     function handleLogin(event) {
-        event.preventDefault();  // Prevent the default form submission behavior
+        event.preventDefault();  // Prevent reloading the page
         
         if (name === "Jimmy") {
-            //setAuth(true);
             nav("/todo");
-        } else { alert("Invalid"); }
+        } else { 
+            alert("Invalid"); 
+        }
     }
-
-    /* if (auth) {
-        return (
-            <Todo />
-        );
-    }*/
 
     return (
         <form method="get" onSubmit={handleLogin}>
@@ -30,10 +22,9 @@ function Login() {
                 type="text" 
                 name="name" 
                 value={name} 
-                //placeholder={phName} 
-                onChange={(e) => setName(e.target.value)} // Update state when input changes
+                placeholder="Name?"
+                onChange={(e) => setName(e.target.value)} // Update useState of "name" when input changes
             />
-            <button type="submit">Login</button>
         </form>
     );
 }
