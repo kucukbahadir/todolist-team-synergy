622adbe0d0743eb1b9b0a3c22573aad8f4a7b0d1
MartenYeager <marten.ter.meer@gmail.com>
2024-09-28 14:02:41 +0200

    Innit sessions using sessionStorage

--- src/pages/login.js
+++ src/pages/login.js
@@ -10,6 +10,11 @@ function Login() {
         event.preventDefault();  // Prevent reloading the page
         
         if (name === "Jimmy") {
+            // sets the user's name in the session storage
+            // TODO: replace the second name with actual data
+            sessionStorage.setItem(name, name);
+            // TODO: when Christian merges add-tasks, change todo to work with dynamic url
+            //nav("/todo/${name}");
             nav("/todo");
         } else { 
             alert("Invalid"); 
