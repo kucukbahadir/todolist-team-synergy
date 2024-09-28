716a811a2d95c1f4715dfe2b614856ee947222b8
MartenYeager <marten.ter.meer@gmail.com>
2024-09-28 13:04:13 +0200

    Added basic login functionality
    Added basic login functionality that will reroute to "/todo" on successful login. Currently only an user by the name "Jimmy" can log in

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,12 +1,41 @@
 import React from "react";
+import {useState} from "react";
+import { useNavigate } from "react-router-dom";
 
-// Due to the router being nested in "App-main", it also takes its CSS, found in "App.css"
-const Login = () => {
+//import Todo from "./todo"
+
+function Login() {
+    const [name, setName] = useState("");
+    //const [auth, setAuth] = useState(false);
+    const nav = useNavigate();
+
+    function handleLogin(event) {
+        event.preventDefault();  // Prevent the default form submission behavior
+        
+        if (name === "Jimmy") {
+            //setAuth(true);
+            nav("/todo");
+        } else { alert("Invalid"); }
+    }
+
+    /* if (auth) {
+        return (
+            <Todo />
+        );
+    }*/
+
     return (
-        <div>
-            <h1>Login page</h1>
-        </div>
+        <form method="get" onSubmit={handleLogin}>
+            <input 
+                type="text" 
+                name="name" 
+                value={name} 
+                //placeholder={phName} 
+                onChange={(e) => setName(e.target.value)} // Update state when input changes
+            />
+            <button type="submit">Login</button>
+        </form>
     );
-};
+}
  
 export default Login;
