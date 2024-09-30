f9c160a765e50a2fd7d3f4fd3b8aad7ad0a435c0
Christian <118530836+faze-chris@users.noreply.github.com>
2024-09-30 10:58:44 +0200

    i added a button to show the form
    it looks better this way

--- src/pages/todo.js
+++ src/pages/todo.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 const Todo = () => {
-  // State to store the list of todos
+  // these are defaulft to do's so that the grid bootstrap dont look weird
   const [todos, setTodos] = useState([
     { id: 1, title: "To do 1", description: "I need to add some things to this page" },
     { id: 2, title: "To do 2", description: "Text" },
@@ -12,96 +12,109 @@ const Todo = () => {
     { id: 7, title: "To do 7", description: "Text" },
     { id: 8, title: "To do 8", description: "Text" },
   ]);
-  //this is for if you want a clean to do screen
-    //   const [todos, setTodos] = useState([]);
 
   const [newTodoTitle, setNewTodoTitle] = useState("");
   const [newTodoDescription, setNewTodoDescription] = useState("");
+  const [showForm, setShowForm] = useState(false); // this is so that you can show the form
 
   // Function to add a new todo
-  const handleAddTodo = (e) => {
+  const buttonAddToDo = (e) => {
     e.preventDefault();
     if (newTodoTitle && newTodoDescription) {
       const newTodo = {
         id: todos.length + 1,
         title: newTodoTitle,
-        description: newTodoDescription
+        description: newTodoDescription,
       };
       setTodos([...todos, newTodo]);
-      setNewTodoTitle(""); 
-      setNewTodoDescription("");
+      setNewTodoTitle(""); // Reset input field
+      setNewTodoDescription(""); // Reset input field
     }
   };
 
   return (
-    <div>
-      <div className="container-fluid bg-black rounded-pill">
-        <br></br>
-        <div className="row justify-content-center align-items-center text-center">
-          <div className="col"></div>
-          <div className="col">
-            <div className="underline p-2">
-              <h2 className="tx-pro">Todo List</h2>
+      <div>
+        <div className="container-fluid bg-black rounded-pill">
+          <br></br>
+          <div className="row justify-content-center align-items-center text-center">
+            <div className="col"></div>
+            <div className="col">
+              <div className="underline p-2">
+                <h2 className="tx-pro">To do List</h2>
+              </div>
             </div>
+            <div className="col"></div>
           </div>
-          <div className="col"></div>
+          <br></br>
         </div>
         <br></br>
-      </div>
-      <br></br>
 
-      {/* Todo adding form */}
-      <div className="container-xl">
-        <div className="row justify-content-center align-items-center text-center">
-          <div className="col-4"></div>
-          <div className="col-4">
-            <form onSubmit={handleAddTodo}>
-              <div className="mb-3">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Todo Title"
-                  value={newTodoTitle}
-                  onChange={(e) => setNewTodoTitle(e.target.value)}
-                  required
-                />
-              </div>
-              <div className="mb-3">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Todo Description"
-                  value={newTodoDescription}
-                  onChange={(e) => setNewTodoDescription(e.target.value)}
-                  required
-                />
-              </div>
-              <button type="submit" className="btn btn-outline-success">
-                Add Todo
-              </button>
-            </form>
-          </div>
-          <div className="col-4"></div>
+        {/* Button to toggle form visibility */}
+        <div className="container text-center">
+          <button
+              className="btn btn-outline-success"
+              onClick={() => setShowForm(!showForm)}
+          >
+            {showForm ? "Hide Form" : "add to do"}
+          </button>
         </div>
-      </div>
-      <br></br>
+        <br />
 
-      {/* Displaying the list of todos */}
-      <div className="container-fluid">
-        <div className="row justify-content-center align-items-center g-2">
-          {todos.map((todo) => (
-            <div key={todo.id} className="col-3">
-              <div className="card rounded-pill">
-                <div className="card-body">
-                  <h4 className="card-title">{todo.title}</h4>
-                  <p className="card-text">{todo.description}</p>
+        {/* this is the form for to do input*/}
+        {showForm && (
+            <div className="container-xl">
+              <div className="row justify-content-center align-items-center text-center">
+                <div className="col-4"></div>
+                <div className="col-4">
+                  <form onSubmit={buttonAddToDo}>
+                    <div className="mb-3">
+                      <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Todo Title"
+                          value={newTodoTitle}
+                          onChange={(e) => setNewTodoTitle(e.target.value)}
+                          required
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Todo Description"
+                          value={newTodoDescription}
+                          onChange={(e) => setNewTodoDescription(e.target.value)}
+                          required
+                      />
+                    </div>
+                    <button type="submit" className="btn btn-outline-success">
+                      Add Todo
+                    </button>
+                  </form>
                 </div>
+                <div className="col-4"></div>
               </div>
             </div>
-          ))}
+        )}
+
+        <br />
+
+        {/*  Displaying the list of todos */}
+        <div className="container-fluid">
+          <div className="row justify-content-center align-items-center g-2">
+            {todos.map((todo) => (
+                <div key={todo.id} className="col-3">
+                  <div className="card rounded-pill">
+                    <div className="card-body">
+                      <h4 className="card-title">{todo.title}</h4>
+                      <p className="card-text">{todo.description}</p>
+                    </div>
+                  </div>
+                </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
