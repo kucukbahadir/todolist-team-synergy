b0b2a82ceb3e603ec09098dee3edd8b8ebbb9c90
Christian <118530836+faze-chris@users.noreply.github.com>
2024-09-30 16:56:38 +0200

    testing to  resolve conflict with merge

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
 
-
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
+    BrowserRouter,
+    Routes,
+    Route,
 } from "react-router-dom";
 
 import Home from "./pages/home";
@@ -12,32 +11,26 @@ import Login from "./pages/login";
 import Todo from "./pages/todo";
 import Detail from "./pages/detail";
 import Completed from "./pages/completed";
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Navbar from "./components/Navbar.js";
+import NoPage from "./pages/NoPage";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className='App-header'>
-        <a href='/login'>Login example</a> <br></br>
-        <a href='/todo'>ToDo list example</a> <br></br>
-        <a href='/detail'>Task detail example</a> <br></br>
-        <a href='/completed'>Completed tasks example</a>        
-      </header>
-      <main className="App-main">
-      <Router>
-        <Routes>
-          <Route exact path="/" element= {<Home />}/>
-          <Route path="/login" element={<Login />}/>
-          <Route path="/todo" element={<Todo />}/>
-          <Route path="/detail" element={<Detail />}/>
-          <Route path="/completed" element={<Completed />}/>
-        </Routes>
-        </Router>
-        
-      </main>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            <main className="App-main">
+                <Routes>
+                    <Route exact path="/" element= {<Home />}/>
+                    <Route path="/login" element={<Login />}/>
+                    <Route path="/todo" element={<Todo />}/>
+                    <Route path="/detail" element={<Detail />}/>
+                    <Route path="/completed" element={<Completed />}/>
+                    <Route path="*" element={<NoPage />} />
+                </Routes>
+            </main>
+        </div>
+    );
 }
 
 export default App;
