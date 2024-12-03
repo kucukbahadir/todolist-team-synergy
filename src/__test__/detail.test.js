de7832893b452d63d31979e61401c9d041aa5390
MartenYeager <marten.ter.meer@gmail.com>
2024-12-03 11:46:59 +0100

    SYNRGY-110| Added localStorage etc

--- src/__test__/detail.test.js
+++ src/__test__/detail.test.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import Detail from '../pages/detail';
+import Detail from '../pages/detail.js';
+import { BrowserRouter } from 'react-router-dom'; // For handling <Navigate>
+import '@testing-library/jest-dom';
+
+
 
 jest.mock('react-router-dom', () => ({
     useNavigate: jest.fn(() => jest.fn()),
@@ -28,9 +32,28 @@ jest.mock('../services/SessionService', () => ({
     },
 }));
 
+beforeEach(() => {
+    // Mock the localstorage
+    //localStorage.setItem("lists", JSON.stringify([{ tasks: [{_id: 'id1', tasks: ["673f20c6978f5452f334fba4", "673f20c6978f5452f334fba5"]}] }]));
+    localStorage.setItem("user", JSON.stringify({ email: "mock@jest.test"}))
+
+    
+    // Set up the getTasks mock to return mockTasks
+    //taskService.getTasks.mockResolvedValue(mockTasks);
+});
+
+afterEach(() => {
+    localStorage.clear();
+})
+
 describe('Detail Page', () => {
     it('renders the detail form and allows updates', async () => {
-        render(<Detail />);
+        render(
+            //<BrowserRouter> {/* Needed for useNavigate */}
+                <Detail />
+            //</BrowserRouter>
+        );
+        console.log(screen);
         expect(screen.getByPlaceholderText(/Title/i)).toBeInTheDocument();
         expect(screen.getByPlaceholderText(/Description/i)).toBeInTheDocument();
 
@@ -38,6 +61,7 @@ describe('Detail Page', () => {
         fireEvent.change(screen.getByPlaceholderText(/Description/i), { target: { value: 'Updated Description' } });
         fireEvent.click(screen.getByText(/Save/i));
 
+        // Cant this better be done with waitFor??
         await new Promise(setImmediate);
         expect(require('../services/taskService').taskService.updateTask).toHaveBeenCalledWith(
             expect.any(String),
