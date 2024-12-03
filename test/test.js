f44d2b12ba3f7bbac3aa33b1cc1c66ecbba938ed
Sameer Hanif <302548743@student.rocmondriaan.nl>
2024-12-03 18:00:32 +0100

    Keep getting the same error on the front end testing

---
+++ test/test.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import React from 'react';
+import { render } from '@testing-library/react'; 
+
+const tasksJimmy = [
+    {   id: 1,
+        title: "To do 1", 
+        description: "Text",
+        dueDate: new Date(),
+        priority: "Low" 
+    },
+];
+
+function Login() {
+    const [name, setName] = useState("");
+    const nav = useNavigate();
+
+    function handleLogin(event) {
+        event.preventDefault();  // Prevent reloading the page
+        
+        if (name === "Jimmy") {
+            // sets the user's name in the session storage
+            console.log(name);
+            sessionStorage.setItem("nameUser", name);
+
+            console.log(tasksJimmy);
+            localStorage.setItem("tasksUser", JSON.stringify(tasksJimmy));
+            // TODO: when Christian merges add-tasks, change todo to work with dynamic url
+            //nav("/todo/${name}");
+            nav("/todo");
+        } else { 
+            alert("Invalid"); 
+        }
+    }
+
+    return (
+        <form method="get" onSubmit={handleLogin}>
+            <input 
+                className="form-control"
+                type="text" 
+                name="name" 
+                value={name} 
+                placeholder="Name?"
+                onChange={(e) => setName(e.target.value)} // Update useState of "name" when input changes
+            />
+        </form>
+    );
+}
+ 
+export default Login;
